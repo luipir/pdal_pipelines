7864851cee83f507fcbfcb5215cfc720426f4a7f
Luigi Pirelli <luipir@gmail.com>
2018-06-22 17:18:55 +0200

    some tests on different filters

---
+++ pipeline_examples/python_pipeline.py
@@ -0,0 +1,10 @@
+def example(ins, outs):
+    
+    print(type(ins))
+    print(len(ins))
+    print(ins)
+    print("*****************")
+
+    # filters.python must return True to tell
+    # PDAL it successfully completed
+    return 
